42f1ddfc0fa3746257f076853f9677c117f791d6
Mahmudur Rahman Salman <rahmansalman855gmail.com>
2023-02-19 01:45:11 +0600

    Quotes generator project added

---
+++ Kanya West/js/script.js
@@ -0,0 +1,13 @@
+
+const loadQuotes = () => {
+    fetch('https://api.kanye.rest/')
+        .then(res => res.json())
+        .then(data => displayQuote(data));
+}
+
+
+const displayQuote = quote => {
+    
+    const blockquote = document.getElementById("quote"); 
+    blockquote.innerText = quote.quote; 
+}
