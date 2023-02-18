5e93e95ad716ff95bd10fddf52baaab90c4d152c
Mahmudur Rahman Salman <rahmansalman855gmail.com>
2023-02-19 04:40:09 +0600

    Random Countries project added

---
+++ Random Countries/js/script.js
@@ -0,0 +1,29 @@
+// Random Countries api https://restcountries.com/v3.1/all
+
+const loadCountries = () => {
+    fetch("https://restcountries.com/v3.1/all")
+        .then(res => res.json())
+        .then(data => displayCountries(data)); 
+}
+
+
+const displayCountries = countries => {
+    const countryContainer = document.getElementById("country-container")
+
+    countries.forEach(country => {
+        const countryDiv = document.createElement("div"); 
+        console.log(country);
+        countryDiv.classList.add("country")
+        countryDiv.innerHTML = ` 
+        <img src="${country.flags.png}" />
+         <h4>Name: ${country.name.common} </h4>
+         <p>Capital: ${country.capital}</p>
+         <p>Area: ${country.area}</p>
+         <p>Continents: ${country.continents}</p>
+         <p>Population: ${country.population}</p>
+        `
+
+        countryContainer.appendChild(countryDiv); 
+    })
+}
+loadCountries(); 
