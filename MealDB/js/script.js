13fbc96d236133c2cd2f96cb4c47931ba2253c1e
Mahmudur Rahman Salman <rahmansalman855gmail.com>
2023-02-19 22:25:22 +0600

    single meal added

--- MealDB/js/script.js
+++ MealDB/js/script.js
@@ -10,32 +10,59 @@ const loadMeals = (search) => {
 
 const displayMeals = meals => {
     const mealsContainer = document.getElementById("meal-container");
-    mealsContainer.innerHTML = ""; 
+    mealsContainer.innerHTML = "";
 
     meals.forEach(meal => {
-        console.log(meal)
+        // console.log(meal)
         const mealDiv = document.createElement("div");
         mealDiv.classList.add('col');
         mealDiv.innerHTML = ` 
-        <div class="card">
+        <div class="card" onclick="loadSingleMeal(${meal.idMeal})">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-            <div class="card-body">
+            <div  class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">${meal.strCategory}</p>
                 <p class="card-text">${meal.strArea}</p>
-                <p class="card-text">${meal.strInstructions.slice(0,300)} ...........read more</p>
+                <p class="card-text">${meal.strInstructions.slice(0, 300)} ...........read more</p>
             </div>
         </div>
-        `; 
-        mealsContainer.appendChild(mealDiv); 
+        `;
+        mealsContainer.appendChild(mealDiv);
     })
 }
 
 
 const searchFood = () => {
-    const searchField = document.getElementById("search-field"); 
-    const searchText = searchField.value; 
+    const searchField = document.getElementById("search-field");
+    const searchText = searchField.value;
     loadMeals(searchText);
-    searchField.value = ""; 
+    searchField.value = "";
 }
- 
+
+const loadSingleMeal = (idMeal) => {
+    console.log(idMeal)
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySingleMeal(data.meals[0]));
+}
+
+const displaySingleMeal = meal => {
+    const singleMealContainer = document.getElementById("single-meal-container");
+    singleMealContainer.innerHTML = ''; 
+
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("card");
+    mealDiv.innerHTML = ` 
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="image">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strCategory}</p>
+                <p class="card-text">${meal.strArea}</p>
+                <p class="card-text">${meal.strInstructions.slice(0, 300)} ...........read more</p>
+            </div>
+    `;
+    singleMealContainer.appendChild(mealDiv);
+}
+
+loadMeals("")
