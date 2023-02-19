29a6918f3c381a7b8f49961f4aa44ebbda22586e
Mahmudur Rahman Salman <rahmansalman855gmail.com>
2023-02-19 06:43:18 +0600

    clickable feature added to random countries

--- Random Countries/js/script.js
+++ Random Countries/js/script.js
@@ -15,15 +15,38 @@ const displayCountries = countries => {
         console.log(country);
         countryDiv.classList.add("country")
         countryDiv.innerHTML = ` 
-        <img src="${country.flags.png}" />
+        
          <h4>Name: ${country.name.common} </h4>
          <p>Capital: ${country.capital}</p>
          <p>Area: ${country.area}</p>
          <p>Continents: ${country.continents}</p>
          <p>Population: ${country.population}</p>
+         <button onclick="loadCountryDetails('${country.cca2}')">Click here</button>
         `
 
         countryContainer.appendChild(countryDiv); 
     })
+}
+
+
+const loadCountryDetails = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`; 
+    console.log(url); 
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data[0])); 
+}
+
+
+
+const displayDetails = country => {
+    console.log(country)
+    const flagContainer = document.getElementById("flag-container"); 
+    flagContainer.innerHTML = ` 
+    <img src="${country.flags.png}" />
+    <h4>Name: ${country.name.common} </h4>
+
+    `
+    
 }
 // loadCountries(); 
